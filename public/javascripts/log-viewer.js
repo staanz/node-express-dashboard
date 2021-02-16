92589b60caac4fc9e861453dac96e0445b028faa
Shanthanu Varma <shanthanuvarma@Zeros-MacBook-Pro.local>
2021-02-16 16:07:00 -0500

    mod 2 8.1

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,6 +1,6 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
 
-connection.onopen(() => {
+connection.onopen = () => {
     connection.send("Hello from the client!");
-});
+};
