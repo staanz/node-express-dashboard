ccdd0fbe5494828691dffa4eb6d775db649e83ae
Shanthanu Varma <shanthanuvarma@Zeros-MacBook-Pro.local>
2021-02-16 16:09:56 -0500

    mod 2 9

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -4,3 +4,7 @@ const logWindow = document.querySelector("#log-window");
 connection.onopen = () => {
     connection.send("Hello from the client!");
 };
+
+connection.onmessage = (event) => {
+  logWindow.innerHTML = event.data;
+};
