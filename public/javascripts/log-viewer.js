227c67f5632331725d319eef82a89d6fd32e4a46
Shanthanu Varma <shanthanuvarma@Zeros-MacBook-Pro.local>
2021-02-16 16:49:04 -0500

    mod 4 test 3,4,5

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,8 +1,12 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
-
+const filePath = document.getElementById("logFilePath");
 connection.onopen = () => {
-    connection.send("Hello from the client!");
+    if (filePath) {
+        connection.send(filePath);
+    } else {
+        connection.send("Hello from the client!");
+    }
 };
 
 connection.onmessage = (event) => {
